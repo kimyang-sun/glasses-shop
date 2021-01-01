140c87792f5c8eaeedabeff550e33bdcc263aa9e
kimyang-sun <berman532@gmail.com>
2021-01-01 21:01:52 +0900

    내 정보 ui 1차 적용

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -14,15 +14,34 @@ const Profile = ({ user }) => {
     <section>
       <h2 className={cx('title')}>내 정보</h2>
       <div className={cx('box')}>
-        <input
-          className={cx('upload')}
-          type="file"
-          accept="image/*"
-          onChange={onFileChange}
-        />
-        <input className={cx('name')} type="text" />
-        <input className={cx('email')} type="text" />
-        <textarea className={cx('message')}></textarea>
+        <div className={cx('row')}>
+          <span>사진</span>
+          <input
+            className={cx('upload')}
+            type="file"
+            accept="image/*"
+            onChange={onFileChange}
+          />
+        </div>
+        <div className={cx('row')}>
+          <span>이메일</span>
+          <p className={cx('email')} type="text">
+            {user.email}
+          </p>
+        </div>
+        <div className={cx('row')}>
+          <span>이름</span>
+          <input className={cx('name')} type="text" />
+        </div>
+
+        <div className={cx('row')}>
+          <span>소개</span>
+          <textarea className={cx('message')}></textarea>
+        </div>
+        <div className={cx('row')}>
+          <button className={cx('save')}>저장하기</button>
+          <button className={cx('logout')}>로그아웃</button>
+        </div>
       </div>
     </section>
   );
