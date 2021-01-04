89dae7daf55461e0e495a158dbd636a62e2c06aa
kimyang-sun <berman532@gmail.com>
2021-01-04 17:17:56 +0900

    pagination 컴포너트 추가 기능 구현

---
+++ src/components/pagination/pagination.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './pagination.module.css';
+import classNames from 'classnames/bind';
+const cx = classNames.bind(styles);
+
+const Pagination = ({ totalPosts, page, paginate, postsPerPage }) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  return (
+    <ul className={cx('pages')}>
+      {pageNumbers.map(number => (
+        <li className={cx('page')}>
+          <button
+            className={cx('pageBtn', { current: page === number })}
+            onClick={() => paginate(number)}
+          >
+            {number}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Pagination;
--- src/pages/products/products.jsx
+++ src/pages/products/products.jsx
@@ -1,7 +1,8 @@
+import Pagination from 'components/pagination/pagination';
 import Product from 'components/product/product';
 import { useCartDispatch } from 'contexts/cart_context';
 import { useProductDispatch, useProductState } from 'contexts/products_context';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import styles from './products.module.css';
 
 const Products = ({ cartRepository, user }) => {
@@ -9,9 +10,25 @@ const Products = ({ cartRepository, user }) => {
   const productDispatch = useProductDispatch();
   const cartDispatch = useCartDispatch();
 
+  // 페이지 관련
+  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지
+  const [postsPerPage, setPostsPerPage] = useState(4); // 페이지당 아이템 수
+
+  const indexOfLast = currentPage * postsPerPage; // 마지막 페이지 순서
+  const indexOfFirst = indexOfLast - postsPerPage; // 첫 페이지 순서
+  const currentPostsSlice = useCallback(
+    posts => {
+      const current = posts.slice(indexOfFirst, indexOfLast);
+      return current;
+    },
+    [indexOfFirst, indexOfLast]
+  );
+  const currentPosts = currentPostsSlice(productState);
+
   // 카트 담기 및 빼기
   const handleAddOrRemove = item => {
     const { id, name, url, price, coupon, cart } = item;
+
     // 카트상태가 담겨있는 상태면 빼야하고 그렇지 않으면 담아야 합니다.
     if (cart) {
       cartDispatch({
@@ -41,7 +58,7 @@ const Products = ({ cartRepository, user }) => {
     <section>
       <h2>상품목록</h2>
       <ul className={styles.list}>
-        {productState.map(product => (
+        {currentPosts.map(product => (
           <Product
             key={product.id}
             id={product.id}
@@ -54,6 +71,12 @@ const Products = ({ cartRepository, user }) => {
           />
         ))}
       </ul>
+      <Pagination
+        totalPosts={productState.length}
+        page={currentPage}
+        paginate={setCurrentPage}
+        postsPerPage={postsPerPage}
+      />
     </section>
   );
 };
